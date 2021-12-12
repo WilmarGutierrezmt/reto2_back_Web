29cb20d383ff71078e8b86a4635e3510bf0695a9
= <=>
2021-12-12 18:56:25 -0500

    first commit

--- src/main/java/reto2_web/Reto2WebApplication.java
+++ src/main/java/reto2_web/Reto2WebApplication.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import reto2_web.interfaces.InterfaceCleaningProductController;
 import reto2_web.interfaces.InterfaceUser;
 
+@Component
 @SpringBootApplication
 public class Reto2WebApplication implements CommandLineRunner {
     @Autowired
